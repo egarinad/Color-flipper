477bb4a802149ecd87635ee75387bc058cd37a75
egarinad <zavadskij76@gmail.com>
2022-02-13 13:19:05 +0300

    before adapt

---
+++ src/random.js
@@ -0,0 +1,18 @@
+const ran = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E",
+    "F"];
+
+const button = document.querySelector('.bg__button');
+let col = document.querySelector('.bg__name');
+
+button.addEventListener('click', () => {
+    let ranColor = "#";
+    for (let i = 0; i < 6; i++) {
+        ranColor += ran[getNum()];
+    }
+    document.body.style.backgroundColor = ranColor;
+    col.innerText = ranColor;
+})
+
+function getNum() {
+    return Math.floor(Math.random() * ran.length);
+}
---
+++ src/script.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "blue", "rgba(133,122,200)", "#f15025"];
+
+
+const button = document.querySelector('.bg__button');
+const col = document.querySelector('.bg__name');
+
+button.addEventListener('click', () => {
+    const randnum = getNum();
+    document.body.style.backgroundColor = colors[randnum];
+    col.innerText = colors[randnum];
+    // сol.style.backgroungColor = colors[random]; why? 
+})
+
+function getNum() {
+    return Math.floor(Math.random() * colors.length);
+}
